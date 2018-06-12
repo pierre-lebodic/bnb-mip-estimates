ace3dc1572a882fb2ec00aa0df103aa5018ddec3
Pierre Le Bodic <pierre.lebodic@monash.edu>
2018-06-12 15:09:57 +1000

    Added multiple labels for learning.

--- sampling/analyzetree.py
+++ sampling/analyzetree.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, r2_score
@@ -8,16 +9,20 @@ import treenode as tn
 class TreeAnalyzer():
 
     def __init__(self, tree):
-        self.features = [tn.TreeNode.getLpValue, tn.TreeNode.getDepth]
-        self.label = tn.TreeNode.getSubTreeSize
+        #self.features = [tn.TreeNode.getLpValue, tn.TreeNode.getDepth]
+        self.features = [tn.TreeNode.getLpValue]
         self.nfeatures = len(self.features)
+        #self.labels = [tn.TreeNode.getSubTreeSize, loglabel(tn.TreeNode.getSubTreeSize)]  
+        #self.labels = [tn.TreeNode.getSubTreeSize]  
+        self.labels = [loglabel(tn.TreeNode.getSubTreeSize)]  
+        self.nlabels = len(self.labels)
         self.root = tree.root
         assert(self.root is not None)
         self.ninnernodes = (self.root.subtreesize)//2
         #X will store the features
         self.X = np.zeros((self.ninnernodes, self.nfeatures))
         #y will store the size of the subtree at that node
-        self.y = np.zeros(self.ninnernodes)
+        self.y = np.zeros((self.ninnernodes, self.nlabels))
         self.analyzetree()
 
     #we do a raversal of the tree and collect labels and features in y and X
@@ -27,7 +32,9 @@ class TreeAnalyzer():
         for node in self.root:
             #we do not take leaves as data points
             if node.isLeaf() is False:
-                self.y[nodeindex] = self.label(node)
+                #we collect all labels
+                for i, l in enumerate(self.labels):
+                    self.y[nodeindex][i] = l(node)
                 #we collect each feature
                 for i, f in enumerate(self.features):
                     self.X[nodeindex][i]= f(node) 
@@ -48,3 +55,5 @@ class TreeAnalyzer():
         # Explained variance score: 1 is perfect prediction
         print('Variance score: %.2f' % r2_score(self.y, ypred))
 
+def loglabel(label):
+    return lambda x: math.log(label(x))
