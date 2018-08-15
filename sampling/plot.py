3e9aab2441f63035acf0e7f46b52a9a7be6d2ff5
Gregor Hendel <hendel@zib.de>
2018-08-15 16:11:13 +0200

    fixes computation of acceleration

--- sampling/plot.py
+++ sampling/plot.py
@@ -9,13 +9,13 @@ def plotEstimates(estimates,stds,SampleMethod,GenClass,treesize,filename,cmdstr,
     upper_ci = [estimates[i] + math.sqrt(1/(1 - confidenceLevel)) * stds[i] for i in range(len(estimates))]
     plt.plot(range(len(estimates)),estimates,SampleMethod.colour+SampleMethod.graphShape,label=str(SampleMethod))
     if( noplot == 0 ):
-        plt.fill_between(range(len(estimates)), lower_ci, upper_ci, color = SampleMethod.colour, alpha = 0.4, label = "$Conf. %2.2f $"% (confidenceLevel) )
+        plt.fill_between(range(len(estimates)), lower_ci, upper_ci, color = SampleMethod.colour, alpha = 0.4)
     plt.xlabel('$k$',fontsize=18)
     plt.ylabel('$E_k$',fontsize=18)
     plt.title("{}".format(filename.rsplit('/',1)[-1]))
     plt.axhline(y=treesize,color='k',linestyle='-')
     plt.ylim([0,treesize*2])
-    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    plt.legend(fontsize=8)
     plt.rc('legend',fontsize=20)
     plt.tight_layout()
     plt.savefig("{}.{}.png".format(filename,cmdstr))
