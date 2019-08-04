c91a1a62048ed6eb11e4442213a707defc491d66
Gregor Hendel <hendel@zib.de>
2019-08-04 19:50:37 +0200

    only generate uniform progress trees

--- sampling/create_test_prob_files.sh
+++ sampling/create_test_prob_files.sh
@@ -4,7 +4,7 @@ ABCFILES=$@
 
 for i in ${ABCFILES[@]}
 do
-    for method in 0 1
+    for method in 1
     do
         echo $i $method
         ./main.py -f $i 100000000 --debug -o --method $method -w
